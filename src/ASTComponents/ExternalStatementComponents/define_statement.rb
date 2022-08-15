2fc73e453f8bffad5a31abebe1782583fc68aeb7
MarkT <Mark_1183@outlook.com>
2022-08-14 19:52:25 -0600

    Move helper classes to define statement file

--- src/ASTComponents/ExternalStatementComponents/define_statement.rb
+++ src/ASTComponents/ExternalStatementComponents/define_statement.rb
@@ -1,7 +1,6 @@
 
 class DefineStatement
     def initialize( newName, explicitTypeDef)
-        #@oldItemComponent = oldItemComponent
         @newNameToken = newName
         @explicitTypeDef = explicitTypeDef
     end
@@ -11,7 +10,6 @@ class DefineStatement
     end
 
     def get_old_type()
-        #@oldItemComponent
         @explicitTypeDef.oldItemComponent()
     end
 
@@ -31,10 +29,8 @@ class DefineStatement
         }
     end
 
-
-
     def _printLiteral
-        old = @explicitTypeDef._printLiteral()#oldItemComponent().getText()
+        old = @explicitTypeDef._printLiteral()
         _new = @newNameToken.getText()
         return "old def: #{old} new def: #{_new}"
     end
@@ -42,5 +38,198 @@ class DefineStatement
     def _printTokType(item_list)
         @explicitTypeDef._printTokType(item_list)
         item_list.append(@newNameToken.getType())
+    end
+end
+
+class KeyValueType
+    def initialize(tokenTypeToken)
+        @tokenTypeToken = tokenTypeToken # several types, which one?
+        @key_token = nil
+        @value_token = nil
+    end
+
+    def add_key_token(key_token)
+        @key_token = key_token
+    end
+
+    def add_value_token(value_token)
+        @value_token = value_token
+    end
+
+    def _printLiteral
+        @tokenTypeToken.getText() + "(" + @key_token.getText() + ":" + @value_token.getText() + ")"
+    end
+
+    def _printTokType(item_list)
+        item_list.append(@tokenTypeToken.getType())
+        item_list.append(@key_token.getType())
+        item_list.append(@value_token.getType())
+    end
+
+    def toJSON()
+
+    end
+end
+
+class LinearType
+    def initialize(tokenTypeToken)
+        @tokenTypeToken = tokenTypeToken # several types, which one?
+        @value_token = nil
+    end
+
+    def add_value_token(value_token)
+        @value_token = value_token
+    end
+
+    def _printLiteral
+        @tokenTypeToken.getText() + "(" + @value_token.getText() + ")"
+    end
+
+    def _printTokType(item_list)
+        item_list.append(@tokenTypeToken.getType())
+        item_list.append(@value_token.getType())
+    end
+
+    def toJSON()
+
+    end
+end
+
+class RenameType
+    def initialize(tokenTypeToken)
+        @tokenTypeToken = tokenTypeToken
+    end
+
+    def _printLiteral
+        @tokenTypeToken.getText()
+    end
+
+    def _printTokType(item_list)
+        item_list.append(@tokenTypeToken.getType())
+    end
+
+    def toJSON()
+        {
+            "literal" => @tokenTypeToken.getText(),
+            "type" => @tokenTypeToken.getType(),
+            "line_number" => @tokenTypeToken.getLine()
+        }
+    end
+end
+
+class ResultType
+    def initialize(tokenTypeToken)
+        @tokenTypeToken = tokenTypeToken # several types, which one?
+        @value_token = nil
+        @error_type_token = nil
+    end
+
+    def add_value_token(value_token)
+        @value_token = value_token
+    end
+
+    def add_error_type_token(error_type_token)
+        @error_type_token = error_type_token
+    end
+
+    def _printLiteral
+        @tokenTypeToken.getText + "(" + @value_token.getText() + "," + @error_type_token.getText() + ")"
+    end
+
+    def _printTokType(item_list)
+        item_list.append(@tokenTypeToken.getType())
+        item_list.append(@value_token.getType())
+        item_list.append(@error_type_token.getType())
+    end
+
+    def toJSON()
+
+    end
+end
+
+class FunctionType
+    def initialize(tokenTypeToken)
+        @tokenTypeToken = tokenTypeToken # several types, which one?
+        @return_type_token = nil
+        @arg_types_list = Array.new()
+    end
+
+    def add_arg_type_token(value_token)
+        @arg_types_list.append(value_token)
+    end
+
+    def add_return_type_token(return_type_token)
+        @return_type_token = return_type_token
+    end
+
+    def _printLiteral
+        args = ""
+        @arg_types_list.each do |arg|
+            args += arg.getText() + ","
+        end
+        args = args.delete_suffix(",")
+        @tokenTypeToken.getText() + "(" + args + ")" + @return_type_token.getText()
+    end
+
+    def _printTokType(item_list)
+        item_list.append(@tokenTypeToken.getType())
+        @arg_types_list.each do |arg|
+            item_list.append(arg.getType())
+        end
+        item_list.append(@return_type_token.getType())
+    end
+
+    def toJSON()
+
+    end
+end
+
+class TypeDef
+    def initialize()
+        @sub_container = nil
+    end
+
+    def addSubType(sub_container)
+        @sub_container = sub_container
+    end
+
+    def addMapValueToken(valueTypeToken)
+        @sub_container.add_value_token(valueTypeToken)
+    end
+
+    def addMapKeyToken(keyTypeToken)
+        @sub_container.add_key_token(keyTypeToken)
+    end
+    
+    def addFunctionReturnTypeToken(funcReturnTypeToken)
+        @sub_container.add_return_type_token(funcReturnTypeToken)
+    end
+
+    def addFunctionArgToken(funcArgTypeToken)
+        @sub_container.add_arg_type_token(funcArgTypeToken)
+    end
+
+    def addResultErrorTypeToken(errorTypeToken)
+        @sub_container.add_error_type_token(errorTypeToken)
+    end
+
+    def addResultTypeToken(resultTypeToken)
+        @sub_container.add_value_token(resultTypeToken)
+    end
+
+    def addLinearTypeToken(linearTypeToken)
+        @sub_container.add_value_token(linearTypeToken)
+    end
+
+    def _printLiteral()
+        @sub_container._printLiteral()
+    end
+
+    def _printTokType(item_list)
+        @sub_container._printTokType(item_list)
+    end
+
+    def toJSON()
+        @sub_container.toJSON()
     end
 end
--- src/Parsing/ExternalStatementParsers/defineparser.rb
+++ src/Parsing/ExternalStatementParsers/defineparser.rb
@@ -3,9 +3,7 @@ require_relative '../../tokentype.rb'
 require_relative '../../ASTComponents/ExternalStatementComponents/define_statement.rb'
 
 class DefineParser
-
     def initialize()
-        #@oldNameToken = nil
         @explicitTypeDef = nil
         @newNameToken = nil
         @linear_types = [
@@ -54,7 +52,6 @@ class DefineParser
 
     def keyValueTypeStep(parser, peekTokType)
         @explicitTypeDef.addSubType(KeyValueType.new(peekTokType))
-        #@explicitTypeDef.addTypeToken(peekTokType)
         parser.discard()
         peekTok = parser.peek()
         if(isEOF(peekTok))
@@ -67,9 +64,7 @@ class DefineParser
     end
 
     def linearContainerTypeStep(parser, peekTokType)
-        #raise Exception.new("Not implemented")
         @explicitTypeDef.addSubType(LinearType.new(peekTokType))
-        #@explicitTypeDef.addTypeToken(peekTokType)
         parser.discard()
         peekTok = parser.peek()
         if(isEOF(peekTok))
@@ -107,7 +102,6 @@ class DefineParser
     end
 
     def resultStep(parser, peekTokType)
-        
         @explicitTypeDef.addSubType(ResultType.new(peekTokType))
         parser.discard()
         peekTok = parser.peek()
@@ -171,8 +165,6 @@ class DefineParser
     end
 
     def functionSignatureStep(parser, peekTokType)
-        #raise Exception.new("Not implemented")
-        
         @explicitTypeDef.addSubType(FunctionType.new(peekTokType))
         parser.discard()
         peekTok = parser.peek()
@@ -354,9 +346,7 @@ class DefineParser
     end
 
     def defineStatement()
-        result = DefineStatement.new(
-            #@oldNameToken, 
-            @newNameToken, @explicitTypeDef)
+        result = DefineStatement.new(@newNameToken, @explicitTypeDef)
         reset()
         return result
     end
@@ -368,219 +358,7 @@ class DefineParser
     end
 
     def reset()
-        #@oldNameToken = nil
         @newNameToken = nil
         @explicitTypeDef = nil
-    end
-end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-class KeyValueType
-    def initialize(tokenTypeToken)
-        @tokenTypeToken = tokenTypeToken # several types, which one?
-        @key_token = nil
-        @value_token = nil
-    end
-
-    def add_key_token(key_token)
-        @key_token = key_token
-    end
-
-    def add_value_token(value_token)
-        @value_token = value_token
-    end
-
-    def _printLiteral
-        @tokenTypeToken.getText() + "(" + @key_token.getText() + ":" + @value_token.getText() + ")"
-    end
-
-    def _printTokType(item_list)
-        item_list.append(@tokenTypeToken.getType())
-        item_list.append(@key_token.getType())
-        item_list.append(@value_token.getType())
-    end
-
-    def toJSON()
-
-    end
-end
-
-class LinearType
-    def initialize(tokenTypeToken)
-        @tokenTypeToken = tokenTypeToken # several types, which one?
-        @value_token = nil
-    end
-
-    def add_value_token(value_token)
-        @value_token = value_token
-    end
-
-    def _printLiteral
-        @tokenTypeToken.getText() + "(" + @value_token.getText() + ")"
-    end
-
-    def _printTokType(item_list)
-        item_list.append(@tokenTypeToken.getType())
-        item_list.append(@value_token.getType())
-    end
-
-    def toJSON()
-
-    end
-end
-
-class RenameType
-    def initialize(tokenTypeToken)
-        @tokenTypeToken = tokenTypeToken
-    end
-
-    def _printLiteral
-        @tokenTypeToken.getText()
-    end
-
-    def _printTokType(item_list)
-        item_list.append(@tokenTypeToken.getType())
-    end
-
-    def toJSON()
-        {
-            "literal" => @tokenTypeToken.getText(),
-            "type" => @tokenTypeToken.getType(),
-            "line_number" => @tokenTypeToken.getLine()
-        }
-    end
-end
-
-class ResultType
-    def initialize(tokenTypeToken)
-        @tokenTypeToken = tokenTypeToken # several types, which one?
-        @value_token = nil
-        @error_type_token = nil
-    end
-
-    def add_value_token(value_token)
-        @value_token = value_token
-    end
-
-    def add_error_type_token(error_type_token)
-        @error_type_token = error_type_token
-    end
-
-    def _printLiteral
-        @tokenTypeToken.getText + "(" + @value_token.getText() + "," + @error_type_token.getText() + ")"
-    end
-
-    def _printTokType(item_list)
-        item_list.append(@tokenTypeToken.getType())
-        item_list.append(@value_token.getType())
-        item_list.append(@error_type_token.getType())
-    end
-
-    def toJSON()
-
-    end
-end
-
-class FunctionType
-    def initialize(tokenTypeToken)
-        @tokenTypeToken = tokenTypeToken # several types, which one?
-        @return_type_token = nil
-        @arg_types_list = Array.new()
-    end
-
-    def add_arg_type_token(value_token)
-        @arg_types_list.append(value_token)
-    end
-
-    def add_return_type_token(return_type_token)
-        @return_type_token = return_type_token
-    end
-
-    def _printLiteral
-        args = ""
-        @arg_types_list.each do |arg|
-            args += arg.getText() + ","
-        end
-        args = args.delete_suffix(",")
-        @tokenTypeToken.getText() + "(" + args + ")" + @return_type_token.getText()
-    end
-
-    def _printTokType(item_list)
-        item_list.append(@tokenTypeToken.getType())
-        @arg_types_list.each do |arg|
-            item_list.append(arg.getType())
-        end
-        item_list.append(@return_type_token.getType())
-    end
-
-    def toJSON()
-
-    end
-end
-
-class TypeDef
-    def initialize()
-        @sub_container = nil
-    end
-
-    def addSubType(sub_container)
-        @sub_container = sub_container
-    end
-
-    def addMapValueToken(valueTypeToken)
-        @sub_container.add_value_token(valueTypeToken)
-    end
-
-    def addMapKeyToken(keyTypeToken)
-        @sub_container.add_key_token(keyTypeToken)
-    end
-    
-    def addFunctionReturnTypeToken(funcReturnTypeToken)
-        @sub_container.add_return_type_token(funcReturnTypeToken)
-    end
-
-    def addFunctionArgToken(funcArgTypeToken)
-        @sub_container.add_arg_type_token(funcArgTypeToken)
-    end
-
-    def addResultErrorTypeToken(errorTypeToken)
-        @sub_container.add_error_type_token(errorTypeToken)
-    end
-
-    def addResultTypeToken(resultTypeToken)
-        @sub_container.add_value_token(resultTypeToken)
-    end
-
-    def addLinearTypeToken(linearTypeToken)
-        @sub_container.add_value_token(linearTypeToken)
-    end
-
-    def _printLiteral()
-        @sub_container._printLiteral()
-    end
-
-    def _printTokType(item_list)
-        @sub_container._printTokType(item_list)
-    end
-
-    def toJSON()
-        @sub_container.toJSON()
     end
 end
